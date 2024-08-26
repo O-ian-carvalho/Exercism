021b1ddf2beb4c826a21c670ecc746fb76592f0c
Ian Carvalho <carvalhoian2509@gmail.com>
2024-08-26 14:31:03 -0300

    international-calling-connoisseur

--- csharp/international-calling-connoisseur/obj/Debug/net8.0/InternationalCallingConnoisseur.AssemblyInfo.cs
+++ csharp/international-calling-connoisseur/obj/Debug/net8.0/InternationalCallingConnoisseur.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("InternationalCallingConnoisseur")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3e277d4ceb5e330156c55c0515609bbd063d242")]
-[assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
-[assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
+[assembly: System.Reflection.AssemblyProductAttribute("InternationalCallingConnoisseur")]
+[assembly: System.Reflection.AssemblyTitleAttribute("InternationalCallingConnoisseur")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
